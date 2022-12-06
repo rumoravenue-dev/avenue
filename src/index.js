4756bbdfa393bcbf89c53fe9847a580010dc2497
Gustavo-mts <gm357191@gmail.com>
2022-12-06 15:16:50 -0300

    first

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import ReactDOM from 'react-dom/client';
-import Home from './Home';
+import Home from './pages/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
