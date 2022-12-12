1aeedae9c4317cd7a09ad604f6f61bab97eee3ad
Amanda Zambelli <amandazambelli@ymail.com>
2022-12-12 16:09:33 -0300

    feat: alteracoes no portfolio e contact

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,7 +1,5 @@
 import React from "react";
-import icon from './images/Vector.svg';
-import sunset from './images/Sunset.svg';
-import Form from '../../components/form/Form';
+import GetInContact from "../../components/getInContact/getInContact";
 import Header from "../../components/header/header";
 import Footer from "../../components/footer/footer";
 import "./contact.css";
@@ -10,33 +8,30 @@ function Contact() {
   return (
     <div>
       <section className="container-main">
-        <Header />
+        {/* <Header /> */}
         <div className="row">
           <div className="col-12 col-sm-2 div-icon">
-            <img src={ icon } alt="sunset" />
+            <img src="/assets/img/contact/Vector.svg" alt="logo" />
           </div>
-          <div className="col-12 col-sm-10 div-somewhere-text">
-            <img className="sunset-img" src={ sunset } alt="sunset" />
+          <div className="col-12 col-sm-10 col-xl-10 col-lg-10 col-md-10 div-somewhere-text">
+            <img className="sunset-img" src="/assets/img/contact/Sunset.svg" alt="sunset" />
             <h1>It's 5pm somewhere</h1>
           </div>
         </div>
       </section>
 
-      <section className="container-main">
+      {/* <section className="container-main">
         <div className="row">
-          <div className="col-12 col-sm-10 div-contactus-text">
-            <img className="sunset-img" src={ sunset } alt="sunset" />
+          <div className="col-12 col-sm-10 col-xl-10 col-lg-10 col-md-10 div-contactus-text">
+            <img className="sunset-img" src="/assets/img/contact/Sunset.svg" alt="sunset" />
             <h2>We're here ready 24/7 feel free to Contact us at anytime.</h2>
-            <p>It's always 5pm somewhere. Get in touch with us and Find The best way below to reach us out, or fill up the form.</p>
+            <p><strong>It's always 5pm somewhere.</strong> Get in touch with us and Find The best way below to reach us out, or fill up the form.</p>
             <button className="btn-start">Start a new project</button>
           </div>
         </div>
       </section>
-
-      <section className="container-main">
-        <Form />
-      </section>
-      <Footer />
+      <GetInContact />
+      <Footer /> */}
     </div>
   );
 }
--- src/pages/portfolio/Portfolio.js
+++ src/pages/portfolio/Portfolio.js
@@ -1,35 +1,17 @@
 import React from "react";
-import TakeALook from "./images/takealook.svg";
-import arrowRight from "./images/Arrowright.svg";
-import arrowLeft from "./images/Arrowleft.svg"
-import Photo1 from "./images/Rectangle1.webp";
-import Photo2 from "./images/Rectangle2.webp";
-import Photo3 from "./images/Rectangle3.webp";
-import Photo4 from "./images/Rectangle4.webp";
 import Header from "../../components/header/header";
+import TakeALook from "../../components/takeALook/takeALook";
 import Footer from "../../components/footer/footer";
 import "./portfolio.css";
 
 function Portfolio() {
+  const home = true;
   return (
-    <div className="main-portfolio">
+    <>
       <Header />
-      <div className="title-container">
-          <h4>portfolio</h4>
-          <img src={TakeALook} alt=""/>
-        <div className="arrow-container">
-          <img src={arrowLeft} alt="arrow-left"/>
-          <img src={arrowRight} alt="arrow-right"/>
-        </div>
-      </div>
-      <div className="portfolio-container">
-        <img src={Photo1} alt="portfolio-1"/>
-        <img src={Photo2} alt="portfolio-2"/>
-        <img src={Photo3} alt="portfolio-3"/>
-        <img src={Photo4} alt="portfolio-4"/>
-      </div>
+      <TakeALook home={home}/>
       <Footer />
-    </div>
+    </>
   );
 }
 
