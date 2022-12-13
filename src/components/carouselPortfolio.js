e5e8d39faa9f6afc3e2d0f8faac1809e5ef6c4be
Amanda Zambelli <amandazambelli@ymail.com>
2022-12-13 12:48:56 -0300

    arrumando carousel

--- src/components/carouselPortfolio.js
+++ src/components/carouselPortfolio.js
@@ -5,22 +5,22 @@ function CarouselPort() {
   return (
     <Carousel>
       <Carousel.Item>
-        <img src='/assets/img/portfolio/Rectangle 49.webp' alt="portfolio-1"/>
-        <img src='/assets/img/portfolio/Rectangle 49.webp' alt="portfolio-2"/>
-        <img src='/assets/img/portfolio/Rectangle 49.webp' alt="portfolio-3"/>
-        <img src='/assets/img/portfolio/Rectangle 49.webp' alt="portfolio-4"/>
+        <img src='/assets/img/portfolio/Rectangle1.webp' alt="portfolio-1"/>
+        <img src='/assets/img/portfolio/Rectangle2.webp' alt="portfolio-2"/>
+        <img src='/assets/img/portfolio/Rectangle3.webp' alt="portfolio-3"/>
+        <img src='/assets/img/portfolio/Rectangle4.webp' alt="portfolio-4"/>
       </Carousel.Item>
       <Carousel.Item>
-        <img src='/assets/img/portfolio/Rectangle 49.webp' alt="portfolio-1"/>
-        <img src='/assets/img/portfolio/Rectangle 49.webp' alt="portfolio-2"/>
-        <img src='/assets/img/portfolio/Rectangle 49.webp' alt="portfolio-3"/>
-        <img src='/assets/img/portfolio/Rectangle 49.webp' alt="portfolio-4"/>
+        <img src='/assets/img/portfolio/Rectangle1.webp' alt="portfolio-1"/>
+        <img src='/assets/img/portfolio/Rectangle2.webp' alt="portfolio-2"/>
+        <img src='/assets/img/portfolio/Rectangle3.webp' alt="portfolio-3"/>
+        <img src='/assets/img/portfolio/Rectangle4.webp' alt="portfolio-4"/>
       </Carousel.Item>
       <Carousel.Item>
-        <img src='/assets/img/portfolio/Rectangle 49.webp' alt="portfolio-1"/>
-        <img src='/assets/img/portfolio/Rectangle 49.webp' alt="portfolio-2"/>
-        <img src='/assets/img/portfolio/Rectangle 49.webp' alt="portfolio-3"/>
-        <img src='/assets/img/portfolio/Rectangle 49.webp' alt="portfolio-4"/>
+        <img src='/assets/img/portfolio/Rectangle1.webp' alt="portfolio-1"/>
+        <img src='/assets/img/portfolio/Rectangle2.webp' alt="portfolio-2"/>
+        <img src='/assets/img/portfolio/Rectangle3.webp' alt="portfolio-3"/>
+        <img src='/assets/img/portfolio/Rectangle4.webp' alt="portfolio-4"/>
       </Carousel.Item>
     </Carousel>
   );
--- src/pages/portfolio/Portfolio.js
+++ src/pages/portfolio/Portfolio.js
@@ -1,7 +1,4 @@
-import React, { useState } from "react";
-import TakeALook from "./images/takealook.svg";
-import arrowRight from "./images/Arrowright.svg";
-import arrowLeft from "./images/Arrowleft.svg"
+import React from "react";
 import Carousel from "../../components/carouselPortfolio";
 import Header from "../../components/header";
 import Footer from "../../components/footer";
@@ -16,16 +13,17 @@ function Portfolio() {
       <div className="row title-container">
         <div className="col-12 col-sm-12">
           <h4>portfolio</h4>
-          <img src={TakeALook} alt=""/>
+          <img src='/assets/img/portfolio/takealook.svg' alt="takeALook"/>
         </div>
-        <div className="col-12 col-sm-12 arrow-container" activeIndex={index} onSelect={handleSelect}>
-          <img src={arrowLeft} alt="arrow-left"/>
-          <img src={arrowRight} alt="arrow-right"/>
+        <div className="col-12 col-sm-12 arrow-container">
+          <img src='/assets/img/portfolio/Arrowleft.svg' alt="arrow-left"/>
+          <img src='/assets/img/portfolio/Arrowright.svg' alt="arrow-right"/>
         </div>
       </div>
       <div className="col-12 col-sm-12 portfolio-container">
         <Carousel />
       </div>
+      { home && <a href="/">See all projects →</a>}
       <Footer />
     </div>
   );
