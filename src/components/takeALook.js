04b9bc17343b6e94104b7a865cb2ffe3afcd2cfb
Gustavo-mts <gm357191@gmail.com>
2022-12-14 13:50:59 -0300

    adicionando componentes ourServices e TakeALook na home

--- src/components/takeALook.js
+++ src/components/takeALook.js
@@ -1,25 +1,25 @@
 import React from "react";
-
+import Carousel from "./carouselPortfolio";
 import "./styles/takeALook.css";
 
 function Portfolio(props) {
+  const home = true;
   return (
-    <div className="main-portfolio">
-      <div className="title-container">
-          <h4>portfolio</h4>
-          <img src="/assets/img/portfolio/takealook.svg" alt="take-a-look"/>
-        <div className="arrow-container">
-          <img src="/assets/img/portfolio/Arrowleft.svg" alt="arrow-left"/>
-          <img src="/assets/img/portfolio/Arrowright.svg" alt="arrow-right"/>
+    <div className="mainPortfolio">
+        <div className="row titleContainer">
+          <div className="col-12 col-sm-12">
+            <h4>portfolio</h4>
+            <img className="takeALook" src='/assets/img/portfolio/takealook.svg' alt="takeALook" />
+          </div>
+          <div className="col-12 col-sm-12 arrowContainer">
+            <img src='/assets/img/portfolio/Arrowleft.svg' alt="arrow-left" />
+            <img src='/assets/img/portfolio/Arrowright.svg' alt="arrow-right" />
+          </div>
         </div>
-      </div>
-      <div className="portfolio-container">
-        <img src="/assets/img/portfolio/Rectangle1.webp" alt="portfolio-1"/>
-        <img src="/assets/img/portfolio/Rectangle2.webp" alt="portfolio-2"/>
-        <img src="/assets/img/portfolio/Rectangle3.webp" alt="portfolio-3"/>
-        <img src="/assets/img/portfolio/Rectangle4.webp" alt="portfolio-4"/>
-      </div>
-      { props.home && <a>See all projects →</a> }
+        <div className="col-12 col-sm-12 portfolioContainer">
+          <Carousel />
+        </div>
+        {home && <a href="/">See all projects →</a>}
     </div>
   );
 }
