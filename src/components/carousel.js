f9f32e6e921f54aebf300f184cb5cec8ce3f2aba
Gustavo-mts <gm357191@gmail.com>
2022-12-20 12:52:00 -0300

    ajustes no carousel

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,19 +1,31 @@
-import React, { useState } from 'react';
-import Carousel from 'react-bootstrap/Carousel';
+import React from 'react';
 import ImageSlider from 'react-image-comparison-slider';
+import ReactSwipe from 'react-swipe';
+
+import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
+
 
 import './styles/carousel.css';
 
 function ControlledCarousel() {
-  const [index, setIndex] = useState(0);
 
-  const handleSelect = (selectedIndex, e) => {
-    setIndex(selectedIndex);
-  };
+
+  let reactSwipeEl;
 
   return (
-    <Carousel activeIndex={index} onSelect={handleSelect} className="carouselMain">
-      <Carousel.Item className='carouselItem'>
+    <div className='carouselHome'>
+      <div className='carouselControls'>
+        <a className='buttonPrev' onClick={() => reactSwipeEl.prev()}><h1><AiOutlineLeft color="white"/></h1></a>
+        <a className='buttonNext' onClick={() => reactSwipeEl.next()}><h1><AiOutlineRight color="white"/></h1></a>
+      </div>
+
+      <ReactSwipe
+        className="carousel"
+        swipeOptions={{ continuous: true }}
+        ref={el => (reactSwipeEl = el)}
+        widthOfSiblingSlidePreview={950}
+      >
+
         <div className="carouselContent">
           <ImageSlider
             image1="assets/img/home/carousel/Atria-Ventures-LP-Presentation.webp"
@@ -24,8 +36,9 @@ function ControlledCarousel() {
             sliderColor="transparent"
           />
         </div>
-      </Carousel.Item>
-    </Carousel>
+
+      </ReactSwipe>
+    </div>
   );
 }
 
