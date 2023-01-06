2ae6db356a1dd4e43c45529673af83e981018953
Amanda Zambelli <amandazambelli@ymail.com>
2023-01-06 12:03:00 -0300

    arrumando responsividade pag about

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -42,7 +42,7 @@ function About() {
           </div>
           <div className='col-12 col-sm-12 col-xl-12 col-lg-12 col-md-12 focusText'>
             <h1>We focus <span className="yellow">on presentations</span> exclusively,</h1>
-            <h4>that’s the reason we are so good at all their aspects – <span className='black'> storytelling, design, delivery and tools </span>. No matter the challenge – from quarterly result presentations to a game-changing IPO, <span className='black'> we can help you achieve your goal </span> .</h4>
+            <h4>that’s the reason we are so good at all their aspects – <span className='black'> storytelling, design, delivery and tools </span>. No matter the challenge – from quarterly result presentations to a game-changing IPO, <span className='black'> we can help you achieve your goal</span>.</h4>
             <h2>WE <span className="yellow">COME THROUGH</span> FOR YOU</h2>
             <p>RELIABLE, QUALITY, AND (REASONABLY) AFFORDABLE.</p>
           </div>
@@ -53,9 +53,9 @@ function About() {
       </section>
 
       <EyeOpening />
-      <Brands />
+      {/* <Brands />
       <GetInContact />
-      <Footer />
+      <Footer /> */}
     </ div>
   )
 }
