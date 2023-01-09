d6c245383cdd9385cec521f40c0f9793ed730387
douglas oliveira <douglasgama0@gmail.com>
2023-01-09 20:36:40 -0300

    pre ajuste carousel

--- src/components/carouselPortfolio.js
+++ src/components/carouselPortfolio.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Modal from './modal';
 import ReactSwipe from 'react-swipe';
 
@@ -26,7 +26,7 @@ import CarouselRyna from './sliders/ryna';
 import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 
 function CarouselPort() {
-  
+
   const [isModalAtria, setIsModalAtria] = useState(false)
   const [isModalTMRW, setisModalTMRW] = useState(false)
   const [isModalTina, setisModalTina] = useState(false)
@@ -50,29 +50,29 @@ function CarouselPort() {
 
   return (
     <>
-      {isModalAtria ?( <Modal onClose={() => setIsModalAtria(false)} > <CarouselAtria/> </Modal>) : null}
-      {isModalTMRW ?( <Modal onClose={() => setisModalTMRW(false)} > <CarouselTMRW /> </Modal>) : null}
-      {isModalTina ?( <Modal onClose={() => setisModalTina(false)} > <CarouselTina /> </Modal>) : null}
-      {isModalRiboli ?( <Modal onClose={() => setisModalRiboli(false)} > <CarouselRiboli/> </Modal>) : null}
+      {isModalAtria ? (<Modal onClose={() => setIsModalAtria(false)}> <CarouselAtria onClose={() => setIsModalAtria(false)} /> </Modal>) : null}
+      {isModalTMRW ? (<Modal onClose={() => setisModalTMRW(false)} > <CarouselTMRW /> </Modal>) : null}
+      {isModalTina ? (<Modal onClose={() => setisModalTina(false)} > <CarouselTina /> </Modal>) : null}
+      {isModalRiboli ? (<Modal onClose={() => setisModalRiboli(false)} > <CarouselRiboli /> </Modal>) : null}
 
-      {isModalNewEngland ?( <Modal onClose={() => setisModalNewEngland(false)} > <CarouselNewEngland/> </Modal>) : null}
-      {isModalHennessy ?( <Modal onClose={() => setisModalHennessy(false)} > <CarouselHennessy/> </Modal>) : null}
-      {isModalInPremisse ?( <Modal onClose={() => setisModalInPremisse(false)} > <CarouselInPremisse/> </Modal>) : null}
-      {isModalGreenHouseWine ?( <Modal onClose={() => setisModalGreenHouseWine(false)} > <CarouselGreenHouseWine/> </Modal>) : null}
+      {isModalNewEngland ? (<Modal onClose={() => setisModalNewEngland(false)} > <CarouselNewEngland /> </Modal>) : null}
+      {isModalHennessy ? (<Modal onClose={() => setisModalHennessy(false)} > <CarouselHennessy /> </Modal>) : null}
+      {isModalInPremisse ? (<Modal onClose={() => setisModalInPremisse(false)} > <CarouselInPremisse /> </Modal>) : null}
+      {isModalGreenHouseWine ? (<Modal onClose={() => setisModalGreenHouseWine(false)} > <CarouselGreenHouseWine /> </Modal>) : null}
 
-      {isModalOmit ?( <Modal onClose={() => setIsModalOmit(false)} > <CarouselOmit/> </Modal>) : null}
-      {isModalBookAble ?( <Modal onClose={() => setisModalBookAble(false)} > <CarouselBookAble/> </Modal>) : null}
-      {isModalPelicargo ?( <Modal onClose={() => setisModalPelicargo(false)} > <CarouselPelicargo/> </Modal>) : null}
-      {isModalGaylordHotels ?( <Modal onClose={() => setisModalGaylordHotels(false)} > <CarouselGaylordHotels/> </Modal>) : null}
+      {isModalOmit ? (<Modal onClose={() => setIsModalOmit(false)} > <CarouselOmit /> </Modal>) : null}
+      {isModalBookAble ? (<Modal onClose={() => setisModalBookAble(false)} > <CarouselBookAble /> </Modal>) : null}
+      {isModalPelicargo ? (<Modal onClose={() => setisModalPelicargo(false)} > <CarouselPelicargo /> </Modal>) : null}
+      {isModalGaylordHotels ? (<Modal onClose={() => setisModalGaylordHotels(false)} > <CarouselGaylordHotels /> </Modal>) : null}
 
-      {isModalLilDan ?( <Modal onClose={() => setIsModalLilDan(false)} > <CarouselLilDan/> </Modal>) : null}
-      {isModalWerx ?( <Modal onClose={() => setisModalWerx(false)} > <CarouselWerx/> </Modal>) : null}
-      {isModalRyna ?( <Modal onClose={() => setisModalRyna(false)} > <CarouselRyna/> </Modal>) : null}
+      {isModalLilDan ? (<Modal onClose={() => setIsModalLilDan(false)} > <CarouselLilDan /> </Modal>) : null}
+      {isModalWerx ? (<Modal onClose={() => setisModalWerx(false)} > <CarouselWerx /> </Modal>) : null}
+      {isModalRyna ? (<Modal onClose={() => setisModalRyna(false)} > <CarouselRyna /> </Modal>) : null}
 
       <div className='controls'>
         <div className='controlsPortfolio'>
-          <a className='buttonPrev' onClick={() => reactSwipeEl.prev()}><h1><AiOutlineLeft/></h1></a>
-          <a className='buttonNext' onClick={() => reactSwipeEl.next()}><h1><AiOutlineRight/></h1></a>
+          <a className='buttonPrev' onClick={() => reactSwipeEl.prev()}><h1><AiOutlineLeft /></h1></a>
+          <a className='buttonNext' onClick={() => reactSwipeEl.next()}><h1><AiOutlineRight /></h1></a>
         </div>
       </div>
 
@@ -85,41 +85,41 @@ function CarouselPort() {
 
         <div className='carouselSlide'>
           <div className='slides'>
-            <img className='slidesIMG' src='/assets/img/portfolio/projects/atria/Atria2.webp' alt="slide-1" onClick={() => setIsModalAtria(true)}/>
-            <img className='slidesIMG' src='/assets/img/sliders/TMRW1.webp' alt="slide-1"  onClick={() => setisModalTMRW(true)}/>
+            <img className='slidesIMG' src='/assets/img/portfolio/projects/atria/Atria2.webp' alt="slide-1" onClick={() => setIsModalAtria(true)} />
+            <img className='slidesIMG' src='/assets/img/sliders/TMRW1.webp' alt="slide-1" onClick={() => setisModalTMRW(true)} />
           </div>
           <div className='slides'>
-            <img className='slidesIMG' src='/assets/img/sliders/TINA1.webp' alt="slide-1"  onClick={() => setisModalTina(true)}/>
-            <img className='slidesIMG' src='/assets/img/sliders/RIBOLI1.webp' alt="slide-1"  onClick={() => setisModalRiboli(true)}/>
-          </div>
-        </div>
-        <div className='carouselSlide'>
-          <div className='slides'>
-            <img className='slidesIMG' src='/assets/img/sliders/NEWENGLAND1.webp' alt="slide-1"  onClick={() => setisModalNewEngland(true)}/>
-            <img className='slidesIMG' src='/assets/img/sliders/HENNESSY1.webp' alt="slide-1"  onClick={() => setisModalHennessy(true)}/>
-          </div>
-          <div className='slides'>
-            <img className='slidesIMG' src='/assets/img/sliders/INPREMISSE1.webp' alt="slide-1"  onClick={() => setisModalInPremisse(true)}/>
-            <img className='slidesIMG' src='/assets/img/sliders/GREENHOUSEWINE1.webp' alt="slide-1"  onClick={() => setisModalGreenHouseWine(true)}/>
+            <img className='slidesIMG' src='/assets/img/sliders/TINA1.webp' alt="slide-1" onClick={() => setisModalTina(true)} />
+            <img className='slidesIMG' src='/assets/img/sliders/RIBOLI1.webp' alt="slide-1" onClick={() => setisModalRiboli(true)} />
           </div>
         </div>
         <div className='carouselSlide'>
           <div className='slides'>
-            <img className='slidesIMG' src='/assets/img/portfolio/projects/omit/Artboard 1 (1).webp' alt="slide-1"  onClick={() => setIsModalOmit(true)}/>
-            <img className='slidesIMG' src='/assets/img/portfolio/projects/bookable/Artboard 1.webp' alt="slide-1"  onClick={() => setisModalBookAble(true)}/>
+            <img className='slidesIMG' src='/assets/img/sliders/NEWENGLAND1.webp' alt="slide-1" onClick={() => setisModalNewEngland(true)} />
+            <img className='slidesIMG' src='/assets/img/sliders/HENNESSY1.webp' alt="slide-1" onClick={() => setisModalHennessy(true)} />
           </div>
           <div className='slides'>
-            <img className='slidesIMG' src='/assets/img/portfolio/projects/pelicargo/Pelicargo1.webp' alt="slide-1"  onClick={() => setisModalPelicargo(true)}/>
-            <img className='slidesIMG' src='/assets/img/portfolio/projects/gaylord/Gaylord_01.webp' alt="slide-1"  onClick={() => setisModalGaylordHotels(true)}/>
+            <img className='slidesIMG' src='/assets/img/sliders/INPREMISSE1.webp' alt="slide-1" onClick={() => setisModalInPremisse(true)} />
+            <img className='slidesIMG' src='/assets/img/sliders/GREENHOUSEWINE1.webp' alt="slide-1" onClick={() => setisModalGreenHouseWine(true)} />
           </div>
         </div>
         <div className='carouselSlide'>
           <div className='slides'>
-            <img className='slidesIMG' src='/assets/img/portfolio/projects/lildan/LILDAN1.webp' alt="slide-1" onClick={() => setIsModalLilDan(true)}/>
-            <img className='slidesIMG' src='/assets/img/portfolio/projects/werx/werx1.webp' alt="slide-1" onClick={() => setisModalWerx(true)}/>
+            <img className='slidesIMG' src='/assets/img/portfolio/projects/omit/Artboard 1 (1).webp' alt="slide-1" onClick={() => setIsModalOmit(true)} />
+            <img className='slidesIMG' src='/assets/img/portfolio/projects/bookable/Artboard 1.webp' alt="slide-1" onClick={() => setisModalBookAble(true)} />
           </div>
           <div className='slides'>
-            <img className='slidesIMG' src='/assets/img/portfolio/projects/ryna/Ryna1.webp' alt="slide-1" onClick={() => setisModalRyna(true)}/>
+            <img className='slidesIMG' src='/assets/img/portfolio/projects/pelicargo/Pelicargo1.webp' alt="slide-1" onClick={() => setisModalPelicargo(true)} />
+            <img className='slidesIMG' src='/assets/img/portfolio/projects/gaylord/Gaylord_01.webp' alt="slide-1" onClick={() => setisModalGaylordHotels(true)} />
+          </div>
+        </div>
+        <div className='carouselSlide'>
+          <div className='slides'>
+            <img className='slidesIMG' src='/assets/img/portfolio/projects/lildan/LILDAN1.webp' alt="slide-1" onClick={() => setIsModalLilDan(true)} />
+            <img className='slidesIMG' src='/assets/img/portfolio/projects/werx/werx1.webp' alt="slide-1" onClick={() => setisModalWerx(true)} />
+          </div>
+          <div className='slides'>
+            <img className='slidesIMG' src='/assets/img/portfolio/projects/ryna/Ryna1.webp' alt="slide-1" onClick={() => setisModalRyna(true)} />
           </div>
         </div>
       </ReactSwipe>
--- src/components/modal.js
+++ src/components/modal.js
@@ -1,17 +1,12 @@
 import React from "react";
-import CloseButton from 'react-bootstrap/CloseButton';
 import "./styles/modal.css";
 
-function Modal({ id = 'modal', onClose = (props) => {}, children }) {
+function Modal({ id = 'modal', onClose = (props) => { }, children }) {
 
-  const handleOutsideClick = (e) => {
-    if(e.target.id === id) onClose();
-  }
 
   return (
-    <div id={id} className="modal" onClick={handleOutsideClick}>
+    <div id={id} className="modal" onClick={() => onClose()}>
       <div className="container">
-        <CloseButton className="close" variant="white" onClick={onClose} />
         <div className="content"> {children} </div>
       </div>
     </div>
