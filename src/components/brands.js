d479d669644b50065c488d34e6a41d82f43f390e
Sandeep <sandeep34@outlook.com>
2023-01-23 20:25:49 -0800

    brand logos

--- src/components/brands.js
+++ src/components/brands.js
@@ -16,83 +16,19 @@ const Brands = () => {
             </div>
           </div>
         </section>
+        <div className="brand-logos">
+          <div><img src="/assets/img/home/partnerships-v2/anheuser-busch.svg" alt="" /></div>
+          <div><img src="/assets/img/home/partnerships-v2/marriott.png" alt="" /></div>
+          <div><img src="/assets/img/home/partnerships-v2/ciroc.svg" alt="" /></div>
+          <div><img src="/assets/img/home/partnerships-v2/rumor.svg" alt="" /></div>
+          <div><img src="/assets/img/home/partnerships-v2/greenhouse.png" alt="" /></div>
+          <div><img src="/assets/img/home/partnerships-v2/appcore.png" alt="" /></div>
+          <div><img src="/assets/img/home/partnerships-v2/hennessy-vsop.png" alt="" /></div>
+          <div><img src="/assets/img/home/partnerships-v2/pelicargo.svg" alt="" /></div>
+          <div><img src="/assets/img/home/partnerships-v2/moet-hennessy-usa.png" alt="" /></div>
+          <div><img src="/assets/img/home/partnerships-v2/balance-bunny.svg" alt="" /></div>
       </div>
-      <section className="brandsPartnerships justify-content-center align-items-center">
-          <div className="row brandsPartnershipsRow justify-content-center align-items-center">
-            <div className="d-lg-block d-none">
-              <div className="row justify-content-center align-items-center partnershipsCards">
-                <div className="col-6 col-sm-6 col-lg-2 cardAnheuserBusch">
-                  <img src="/assets/img/home/Partnerships/AnheuserBusch.webp" alt="" />
-                </div>
-                <div className="col-6 col-sm-6 col-lg-2 cardMarriott">
-                  <img src="/assets/img/home/Partnerships/Marriott.webp" alt="" />
-                </div>
-                <div className="col-6 col-sm-6 col-lg-2 cardCiroc">
-                  <img src="/assets/img/home/Partnerships/Ciroc.webp" alt="" />
-                </div>
-                <div className="col-6 col-sm-6 col-lg-2 cardRumor">
-                  <img src="/assets/img/home/Partnerships/Rumor.webp" alt="" />
-                </div>
-                <div className="col-6 col-sm-6 col-lg-2  cardLogoGreen">
-                  <img src="/assets/img/home/Partnerships/GH-Logo-Green.webp" alt="" />
-                </div>
-              </div>
-              <div className="row justify-content-center align-items-center partnershipsCards">
-                <div className="col-6 col-sm-6 col-lg-2 cardAppcore">
-                  <img src="/assets/img/home/Partnerships/Appcore.webp" alt="" />
-                </div>
-                <div className="col-6 col-sm-6 col-lg-2 cardHennessyVSOP">
-                  <img src="/assets/img/home/Partnerships/Hennessy-VSOP.webp" alt="" />
-                </div>
-                <div className="col-6 col-sm-6 col-lg-2 cardPelicargo">
-                  <img src="/assets/img/home/Partnerships/Pelicargo.webp" alt="" />
-                </div>
-                <div className="col-6 col-sm-6 col-lg-2 cardMoet">
-                  <img src="/assets/img/home/Partnerships/Moet-Hennessy-USA.webp" alt="" />
-                </div>
-                <div className="col-6 col-sm-6 col-lg-2 cardBalanceBunny">
-                  <img src="/assets/img/home/Partnerships/Balance-Bunny.webp" alt="" />
-                </div>
-              </div>
-            </div>
-
-            <div class="d-lg-none d-block">
-              <div className="row justify-content-center align-items-center partnershipsCards">
-                <div className="col-6 col-sm-6 col-lg-2 cardAnheuserBusch">
-                  <img src="/assets/img/home/Partnerships/AnheuserBusch.webp" alt="" />
-                </div>
-                <div className="col-6 col-sm-6 col-lg-2 cardMarriott">
-                  <img src="/assets/img/home/Partnerships/Marriott.webp" alt="" />
-                </div>
-                <div className="col-6 col-sm-6 col-lg-2 cardCiroc">
-                  <img src="/assets/img/home/Partnerships/Ciroc.webp" alt="" />
-                </div>
-                <div className="col-6 col-sm-6 col-lg-2 cardRumor">
-                  <img src="/assets/img/home/Partnerships/Rumor.webp" alt="" />
-                </div>
-                <div className="col-6 col-sm-6 col-lg-2  cardLogoGreen">
-                  <img src="/assets/img/home/Partnerships/GH-Logo-Green.webp" alt="" />
-                </div>
-                <div className="col-6 col-sm-6 col-lg-2 cardAppcore">
-                  <img src="/assets/img/home/Partnerships/Appcore.webp" alt="" />
-                </div>
-                <div className="col-6 col-sm-6 col-lg-2 cardHennessyVSOP">
-                  <img src="/assets/img/home/Partnerships/Hennessy-VSOP.webp" alt="" />
-                </div>
-                <div className="col-6 col-sm-6 col-lg-2 cardPelicargo">
-                  <img src="/assets/img/home/Partnerships/Pelicargo.webp" alt="" />
-                </div>
-                <div className="col-6 col-sm-6 col-lg-2 cardMoet">
-                  <img src="/assets/img/home/Partnerships/Moet-Hennessy-USA.webp" alt="" />
-                </div>
-                <div className="col-6 col-sm-6 col-lg-2 cardBalanceBunny">
-                  <img src="/assets/img/home/Partnerships/Balance-Bunny.webp" alt="" />
-                </div>
-              </div>
-            </div>
-
-          </div>
-        </section>
+      </div>
     </>
   );
 };
