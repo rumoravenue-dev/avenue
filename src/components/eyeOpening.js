7dfeba7d5767e0a0024f6f2fb140f354cfe55f31
Sandeep <sandeep34@outlook.com>
2023-01-26 12:05:33 -0800

    fix eye section

--- src/components/eyeOpening.js
+++ src/components/eyeOpening.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import './styles/eyeOpening.css';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
+import Spacer from './spacer';
 
 export default function EyeOpening() {
   useEffect(() => {
@@ -10,34 +11,38 @@ export default function EyeOpening() {
 
   return (
     <section className="eyeSection container-fluid">
-      <div className='row justify-content-center'>
-        <div className='col col-md-4'>
-          <div className="col box-text" data-aos="fade-up">
-            <h1 className='title-eye'>
-              EYE
-            </h1>
-            <h1 className='title-eye'>
-              OPENING
-            </h1>
-            <p className='description-text-eye'>
-              Whether the presentation needs to convince or educate it always has to change audience perception. We seek insights to make decks unexpectedly enlightening for our audience.
-            </p>
+      <div className='container-wide'>
+        <Spacer height={200} />
+        <div className='row justify-content-between'>
+          <div className='col col-md-4'>
+            <div className="col box-text" data-aos="fade-up">
+              <h1 className='title-eye'>
+                EYE
+              </h1>
+              <h1 className='title-eye'>
+                OPENING
+              </h1>
+              <p className='description-text-eye'>
+                Whether the presentation needs to convince or educate it always has to change audience perception. We seek insights to make decks unexpectedly enlightening for our audience.
+              </p>
+            </div>
+            <div className="col eyeCatching" data-aos="fade-up">
+              <h1 className='title-eye'>
+                EYE
+              </h1>
+              <h1 className='title-eye'>
+                CATCHING
+              </h1>
+              <p className='description-text-eye'>
+                The presentation helps to see what's hidden, unseen, or simply never known before. We use design to drive their attention, focus them to clearly see, and help them feel the message.
+              </p>
+            </div>
           </div>
-          <div className="col eyeCatching" data-aos="fade-up">
-            <h1 className='title-eye'>
-              EYE
-            </h1>
-            <h1 className='title-eye'>
-              CATCHING
-            </h1>
-            <p className='description-text-eye'>
-              The presentation helps to see what's hidden, unseen, or simply never known before. We use design to drive their attention, focus them to clearly see, and help them feel the message.
-            </p>
+          <div className='col col-md-auto container-eye'>
+            <img className="img-eye" src="/assets/img/about/eye.gif" alt="eye" />
           </div>
         </div>
-        <div className='col col-md-auto container-eye'>
-          <img className="img-eye" src="/assets/img/about/eye.gif" alt="eye" />
-        </div>
+        <Spacer height={200} />
       </div>
     </section>
   )
