b81b7c5aec48831c99d42d9790b8f21c98cae63b
Ashish Jangra <ashish.perisun@gmail.com>
2023-03-02 22:14:18 +0530

    structure change

--- src/components/singlePortfolioIndustryAndTools.js
+++ src/components/singlePortfolioIndustryAndTools.js
@@ -7,7 +7,7 @@ const SinglePortfolioIndstryAndTools = (props) => {
   let { tools } = props.tools;
   return (
     <>
-      <div className="col-12 col-sm-8 portfolioDetails industry">
+      <div className="col-12 col-sm-7 portfolioDetails industry">
         <h3>Industry</h3>
         <Spacer height={20} />
         <p>
@@ -16,19 +16,21 @@ const SinglePortfolioIndstryAndTools = (props) => {
           )}
         </p>
       </div>
-      <div className="col-12 col-sm-4 portfolioDetails">
-        <h3>Tools Used</h3>
-        <Spacer height={20} />
-        <ul className="tech-stack">
-          {tools.map((value,key) =>
-            <li>
-              <img
-                  src= {"/assets/img/portfolio/projects/tools/"+value+".svg" }
-                  alt={value}
-              />
-            </li>
-          )}
-        </ul>
+      <div className="col-12 col-sm-7 portfolioDetails d-flex justify-content-end">
+        <div className="d-inline-block">
+          <h3>Tools Used</h3>
+          <Spacer height={20} />
+          <ul className="tech-stack">
+            {tools.map((value,key) =>
+              <li>
+                <img
+                    src= {"/assets/img/portfolio/projects/tools/"+value+".svg" }
+                    alt={value}
+                />
+              </li>
+            )}
+          </ul>
+        </div>
       </div>
       <Spacer height={120} />
     </>
